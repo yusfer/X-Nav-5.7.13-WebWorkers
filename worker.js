5538574ac251e9f8b43b1b42a4d1fa2cfff8a37c
yusfer <yustasfernando@gmail.com>
2015-03-23 14:13:38 +0100

    Realizado el ejercicio

---
+++ worker.js
@@ -0,0 +1,19 @@
+self.onmessage = function(event) {
+	sacaPrimos(event.data)
+}
+
+function sacaPrimos(numero) {
+  var n = 1;
+  primelist = "";
+  search: while (n<parseInt(numero)) {
+    n += 1;
+    for (var i = 2; i <= Math.sqrt(n); i += 1)
+      if (n % i == 0)
+        continue search;
+    // found a prime!
+    primelist += " " + n;
+  }
+  self.postMessage(primelist)
+}
+
+
